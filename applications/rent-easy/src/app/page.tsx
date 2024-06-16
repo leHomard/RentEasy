94f5dbea3c1a2ce6d507df148c04ddb7455f64ad
Omar Harras <omar.harras.externe@foncia.com>
2024-06-16 15:40:23 +0200

    chore: set up shadcn

--- applications/rent-easy/src/app/page.tsx
+++ applications/rent-easy/src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Rent Easy</div>
+      <form>
+        <input placeholder="Lessor" name="lessor" />
+        <Button type="submit">Add Lessor</Button>
+      </form>
     </main>
   );
 }
